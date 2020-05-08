b379ba4ce177779b334298ed3d424edab8299df6
tom <tom@sapletta.com>
2020-05-08 11:49:39 +0200

    check if file or software exist

--- download.bat
+++ download.bat
@@ -1,19 +1,16 @@
 @echo off
-:: silent install nodejs on windows console
-:: https://www.ibm.com/support/knowledgecenter/SSZUMP_7.2.1/install_grid_sym/install_silent.html
-
 setlocal
 
 set URL=https://getcomposer.org/installer
 set FILENAME=composer-setup.php
-set LOG=node-log.txt
-set INSTALLDIR=C:\nodejs\
+set LOG=composer-log.txt
+set INSTALLDIR=C:\
 
 @echo on
 
 curl.bat %URL% %FILENAME%
 :: php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
-echo file %NODEJS_FILENAME% is downloaded!
+echo file %FILENAME% is downloaded!
 
 endlocal
 dir
